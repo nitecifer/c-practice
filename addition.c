a0e2ad1fbce0559775005a3c23229bbbe539aa34
nitecifer <nitegupta2004@gmail.com>
2023-04-27 19:40:34 +0530

    program foraddtion of three variable

--- addition.c
+++ addition.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 int main(){
-    int a =5,b=7;
-    int c=0;
-    c=a+b;
-    printf("%d",c);
+    int a =5,b=7 ,c=6;
+    int d=0;
+    d=a+b+c;
+    printf("%d",d);
     return 0;
 }
